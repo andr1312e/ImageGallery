0b69b8df2ede08457025c69f65585741718b7d01
Andre Pilikin <andr1312e@outlook.com>
2020-02-20 12:51:35 +0300

    1

---
+++ ImageGallery/Controllers/RoleAdminController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Controllers
+{
+    public class RoleAdminController:Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public RoleAdminController(RoleManager<IdentityRole> _roleManager)
+        {
+            roleManager = _roleManager;
+        }
+        public ViewResult Index() => View(roleManager.Roles);
+        public IActionResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string Name)
+        {
+            if (ModelState.IsValid)
+            {
+                if(!await roleManager.RoleExistsAsync(Name))
+                {
+                    IdentityResult result = await roleManager.CreateAsync(new IdentityRole(Name));
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            return View(Name);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError identityError in result.Errors)
+            {
+                ModelState.AddModelError("", identityError.Description);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult identityResult = await roleManager.DeleteAsync(role);
+                if (identityResult.Succeeded)
+                {
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(identityResult);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role found");
+            }
+            return View("Index", roleManager.Roles);
+        }
+    }
+}
---
+++ ImageGallery/Infrastrucure/RoleUsersTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using CustomIdentityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Infrastrucure
+{
+    [HtmlTargetElement("td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper:TagHelper
+    {
+        private UserManager<AppUser> UserManager;
+        private RoleManager<IdentityRole> RoleManager;
+        public RoleUsersTagHelper(UserManager<AppUser> user, RoleManager<IdentityRole> role)
+        {
+            UserManager = user;
+            RoleManager = role;
+        }
+        [HtmlAttributeName("identity-role")]
+        public string Role { set; get; }
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            IdentityRole identityRole = await RoleManager.FindByIdAsync(Role);
+            if (identityRole != null)
+            {
+                foreach (var user in UserManager.Users)
+                {
+                    if (user != null && await UserManager.IsInRoleAsync(user, identityRole.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "No users" : string.Join(", ", names));
+        }
+
+    }
+}
