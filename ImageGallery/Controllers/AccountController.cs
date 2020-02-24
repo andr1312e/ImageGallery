7ebbf701263b070f47cf500f5cf7a934d79bd593
Andre Pilikin <andr1312e@outlook.com>
2020-02-24 20:17:37 +0300

    Fix Bugs, Add Edit + Delete Images

--- ImageGallery/Controllers/AccountController.cs
+++ ImageGallery/Controllers/AccountController.cs
@@ -16,16 +16,22 @@ namespace ImageGallery.Controllers
     {
         private UserManager<AppUser> _userManager;
         private SignInManager<AppUser> _signInManager;
+        private IUserValidator<AppUser> _userValidator;
+        private IPasswordValidator<AppUser> _passwordValidator;
+        private IPasswordHasher<AppUser> _passwordHasher;
         [Authorize]
         public async Task<IActionResult> Logout() 
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "ImageGallery");
         }
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IUserValidator<AppUser> userValidator, IPasswordValidator<AppUser> passwordValidator, IPasswordHasher<AppUser> passwordHasher)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _userValidator = userValidator;
+            _passwordValidator = passwordValidator;
+            _passwordHasher = passwordHasher;
         }
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
@@ -45,6 +51,54 @@ namespace ImageGallery.Controllers
                 {
                     return RedirectToAction("ImageGallery","Index");
                 }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string Email, string password)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = Email;
+                IdentityResult validEmail = await _userValidator.ValidateAsync(_userManager, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (password != null && password.Length >= 1)
+                {
+                    validPass = await _passwordValidator.ValidateAsync(_userManager, user, password);
+                }
+                if (validPass.Succeeded)
+                {
+                    user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                }
+                if ((validEmail.Succeeded && validPass == null) || (validEmail.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "ImageGallery");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+
+        }
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
         [HttpPost]
         [AllowAnonymous]
